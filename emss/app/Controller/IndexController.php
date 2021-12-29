381c9d39bab9a0594776900e83267d4eb8f2490d
Tran Thi Thu Thanh <kindaichicao@gmail.com>
2021-12-29 21:46:58 +0700

    [Modified]: home

--- emss/app/Controller/IndexController.php
+++ emss/app/Controller/IndexController.php
@@ -10,7 +10,7 @@ class IndexController extends Controller
     public function __construct()
     {
         parent::__construct();
-        Auth::checkAuthentication();
+        //Auth::checkAuthentication();
     }
 
     // Trang mặc định của web
