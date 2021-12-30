a8ab0b5f2dd1864dc7453b55e1725274bca22332
Anh Quân <phananhquan48@gmail.com>
2021-12-30 18:19:29 +0700

    [Add]: Update address

--- emss/app/Controller/DiaDiemController.php
+++ emss/app/Controller/DiaDiemController.php
@@ -20,8 +20,27 @@ class DiaDiemController extends Controller
         Auth::checkAuthentication();
         $this->View->render('diadiem/index');
     }
-    public function getOneByID()
-    {
+    public function getOneByID(){
+        Auth::checkAuthentication();
+        $id = Request::post('id');
+        $kq = DiaDiemModel::getOneByID($id);
+        $response = ['thanhcong' => false];
+        if ($kq == null) {
+            $response['thanhcong'] = false;
+        } else {
+            $response['ma_dia_diem'] = $kq->ma_dia_diem;
+            $response['ten_dia_diem'] = $kq->ten_dia_diem;
+            $response['tp_tinh'] = $kq->tp_tinh;
+            $response['quan_huyen'] = $kq->quan_huyen;
+            $response['phuong_xa'] = $kq->phuong_xa;
+            $response['ap_thon'] = $kq->ap_thon;
+            $response['so_nha'] = $kq->so_nha;
+            $response['phan_loai'] = $kq->phan_loai;
+            $response['suc_chua'] = $kq->suc_chua;
+            $response['so_luong_trong'] = $kq->so_luong_trong;
+            $response['thanhcong'] = true;
+        }
+        $this->View->renderJSON($response);
     }
     public function add(){
         Auth::checkAuthentication();
@@ -51,8 +70,24 @@ class DiaDiemController extends Controller
         $data = DiaDiemModel::getAllPagination($search, $search2, $page, $rowsPerPage);
         $this->View->renderJSON($data);
     }
-    public function update()
-    {
+    public function update(){
+        Auth::checkAuthentication();
+        //Auth::ktraquyen("CN02");
+        $ma = Request::post('upma');
+        $ten = Request::post('upten');
+        $tinh = Request::post('uptinh');
+        $huyen = Request::post('uphuyen');
+        $xa = Request::post('upxa');
+        $thon = Request::post('upthon');
+        $sonha = Request::post('upsonha');
+        $loai = Request::post('uploai');
+        $succhua = Request::post('upsucchua');
+        $trong = Request::post('uptrong');
+        $kq = DiaDiemModel::update($ma,$ten, $tinh, $huyen , $xa, $thon, $sonha, $loai, $succhua, $trong);
+        $response = [
+            'thanhcong' => $kq
+        ];
+        $this->View->renderJSON($response);
     }
     public function delete()
     {
